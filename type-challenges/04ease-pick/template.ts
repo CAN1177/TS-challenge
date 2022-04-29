b63b98be26c7e29cd051d7476bbdc260ae7838af
CAN7 <39960526+CAN1177@users.noreply.github.com>
2022-04-29 23:08:08 +0800

    feat: ts类型体操环境搭建+04pick实现

---
+++ type-challenges/04ease-pick/template.ts
@@ -0,0 +1,18 @@
+// type MyPick<T, K> = any;
+
+type MyPick<T, K extends keyof T> = {
+	[P in K]: T[P]
+}
+
+
+// js 实现
+// function myPick(todo, keys) {
+//   const obj = {};
+//   keys.forEach((key) => {
+//     if (key in todo) {
+//       obj[key] = todo[key];
+//     }
+//     obj[key] = todo[key];
+//   });
+//   return obj;
+// }
