795ecbd693ee4c2864d7cbc57a51f21063777828
CAN7 <39960526+CAN1177@users.noreply.github.com>
2022-04-30 14:51:02 +0800

    fix: TupleToObject实现bug修复

--- type-challenges/11tupleToObject/template.ts
+++ type-challenges/11tupleToObject/template.ts
@@ -1,13 +1,10 @@
-type TupleToObject<T extends readonly any[]> = {
-	[P in T[number]]: P
-}
-
+type TupleToObject<T extends readonly (number | string | symbol)[]> = {
+  [V in T[number]]: V;
+};
 
 // typeof 操作符可以用来获取一个变量或对象的类型
-// as const 类型断言（所有只读）
+// as const 类型断言（所有）
 
-const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
 
-type result = TupleToObject<typeof tuple> 
-
-
+type result = TupleToObject<typeof tuple>;
