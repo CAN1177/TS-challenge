80ecad505747546a469c578ee36ffaf5a9bd0930
CAN7 <39960526+CAN1177@users.noreply.github.com>
2022-04-30 20:38:35 +0800

    feat: awaited

---
+++ type-challenges/189awaited/template.ts
@@ -0,0 +1,14 @@
+// type MyAwaited = any
+
+// infer X 类似未知数X
+type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer X>
+  ? X extends Promise<unknown>
+    ? MyAwaited<X>
+    : X
+  : never;
+
+/*
+	infer 
+	1、只能在 条件类型里面使用
+	2、设置未知数 变量 
+	*/
