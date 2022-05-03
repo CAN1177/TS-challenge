921b192cc7ca228f042ad16e72575240b33f2abe
CAN7 <39960526+CAN1177@users.noreply.github.com>
2022-05-03 15:15:40 +0800

    feat: omit实现

---
+++ medium-type-challenge/03omit/template.ts
@@ -0,0 +1,18 @@
+// type MyOmit<T, K> = any
+
+type MyOmit<T, K extends keyof T> = {
+  [P in keyof T as P extends K ? never : P]:T[P]
+}
+
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+type TodoPreview = MyOmit<Todo, 'description' | 'title'>
+
+const todo3: TodoPreview = {
+  completed: false,
+}
