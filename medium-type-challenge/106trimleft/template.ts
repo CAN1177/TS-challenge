bbb2af26a0002f4391c89db5cd1a13df097f4f41
CAN7 <39960526+CAN1177@users.noreply.github.com>
2022-05-07 16:55:38 +0800

    feat: trimLeft + Lookup

---
+++ medium-type-challenge/106trimleft/template.ts
@@ -0,0 +1,7 @@
+// type TrimLeft<S extends string> = any
+
+type TrimLeft<S extends string> = S extends `${" " | "\n" | "\t"}${infer Rest}`
+  ? TrimLeft<Rest>
+  : S;
+
+type trimed = TrimLeft<"  Hello World  ">; // 应推导出 'Hello World  '
---
+++ medium-type-challenge/62Lookup/template.ts
@@ -0,0 +1,20 @@
+// type LookUp<U, T> = any
+
+type LookUp<U extends { type: string }, T extends U["type"]> = U extends {
+  type: T;
+}
+  ? U
+  : never;
+
+interface Cat {
+  type: "cat";
+  breeds: "Abyssinian" | "Shorthair" | "Curl" | "Bengal";
+}
+
+interface Dog {
+  type: "dog";
+  breeds: "Hound" | "Brittany" | "Bulldog" | "Boxer";
+  color: "brown" | "white" | "black";
+}
+
+type MyDog = LookUp<Cat | Dog, "dog">; // expected to be `Dog`
