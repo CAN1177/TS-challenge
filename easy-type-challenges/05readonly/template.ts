d581c3fcd27ef13b1050d97bb91711d64e50e525
21Silva <21silva@21SilvadeMacBook-Pro.local>
2024-04-03 21:05:47 +0800

    feat:easy-pick

--- easy-type-challenges/05readonly/template.ts
+++ easy-type-challenges/05readonly/template.ts
@@ -1,30 +1,28 @@
 type MyReadonly<T> = {
-	readonly [key in keyof T]: T[key]
-}
+  readonly [key in keyof T]: T[key];
+};
 
 interface Todo {
-  title: string
-  description: string
+  title: string;
+  description: string;
 }
-
-
 
 // js实现
 
-function readonly(obj) {
-	const result = {}
+// function readonly(obj) {
+//   const result = {};
 
-	for( const key in obj ) {
-		result["readonly" + key] = obj[ key ]
-	}
-	return result
-}
+//   for (const key in obj) {
+//     result["readonly" + key] = obj[key];
+//   }
+//   return result;
+// }
 
-// 测试🌰
-const todo: MyReadonly<Todo> = {
-  title: "Hey",
-  description: "foobar"
-}
+// // 测试🌰
+// const todo: MyReadonly<Todo> = {
+//   title: "Hey",
+//   description: "foobar",
+// };
 
-todo.title = "Hello" // Error: cannot reassign a readonly property
-todo.description = "barFoo" // Error: cannot reassign a readonly property
+// todo.title = "Hello"; // Error: cannot reassign a readonly property
+// todo.description = "barFoo"; // Error: cannot reassign a readonly property
